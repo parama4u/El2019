e48e37a7bb535c466a02ea7d7d8b65e073e98b2c
Param <parama4u@gmail.com>
2019-03-19 17:13:12 +0900

    Updated

--- data/crawler.py
+++ data/crawler.py
@@ -3,17 +3,47 @@ Created on Mar 19, 2019
 
 @author: param
 '''
+import requests
 
 class Crawler(object):
-    '''
-    classdocs
-    '''
+    
+    
+    def myDict(self,txt):
+        try:
+            res={}
+            res[txt.split('=')[0]]=(txt.split('=')[1]).replace('>','')
+            return res
+        except IndexError:
+            pass
+    
+    
+    def __init__(self, txt):
+        
+#         for line in txt:
+#             print(line +',' + str(ord(line)))
+#         
+        for line in txt.split(chr(10)):
+            blk=line.split()
+            tag=blk[0]split.replace('<','').replace('!','')
+            vals=blk[1:]
+            if len(blk)>2:
+                self.__dict__[tag]={}
+                for d in vals:
+                    self.__dict__[tag].update(self.myDict(d))
+            
+            
+        pass
+    
+        
 
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
 
 
+lkURL='https://en.wikipedia.org/wiki/List_of_constituencies_of_the_Lok_Sabha'
 
+obj=requests.get(lkURL).text
+
+res=Crawler(obj)
+
+
+pass
