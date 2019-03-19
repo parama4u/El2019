46d4c917645849b6ea1795b22a456c92185ab923
Param <parama4u@gmail.com>
2019-03-19 12:34:50 +0900

    initial push

---
+++ data/crawler.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 19, 2019
+
+@author: param
+'''
+
+class Crawler(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+
+
---
+++ runme.py
@@ -0,0 +1,7 @@
+'''
+Created on Mar 19, 2019
+@author: param
+'''
+
+if __name__ == '__main__':
+    pass
