b14c592ac52697e2404f2539b9d4429ef33c8c3a
parama4u <parama4u@gmail.com>
2019-03-27 02:42:40 +0900

    Cleanup on CSV and DataReader in Pandas

--- runme.py
+++ runme.py
@@ -3,5 +3,30 @@ Created on Mar 19, 2019
 @author: param
 '''
 
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+class DataReader:
+    
+    @staticmethod
+    def test():
+        data=pd.read_csv('data//loksabha.csv',delimiter=',')
+        print(data)
+        
+    @staticmethod    
+    def tn_by_party():
+        data=pd.read_csv('data//loksabha.csv',delimiter=',')
+        #tn=data[data.NAME.str.contains('Tamil')][data.Position<4][['Year','PARTY','PCNAME','Position']]
+        tn=data[data.NAME.str.contains('Tamil')][data.Position<4][['Year','PARTY','PCNAME','Position','CVOTES']]
+        tn2=tn.groupby('PARTY').cumsum()
+        for r in tn2:
+            print(r)
+        
+        
+    
+
+
 if __name__ == '__main__':
-    pass
+    DataReader.tn_by_party()
